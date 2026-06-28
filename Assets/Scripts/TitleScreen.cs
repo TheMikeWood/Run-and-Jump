85bd246a4588881532d80f0a80087a95cff72e22
Michael Woodard <mr.m.woodard@gmail.com>
2026-06-28 07:40:43 -0400

    touch screen changes

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,38 +1,31 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour
 {
     [Header("Title Text")]
     public TextMeshProUGUI titleText;
     public TextMeshProUGUI taglineText;
-    public TextMeshProUGUI pressAnyKeyText;
 
     [Header("Title Screen Objects")]
     public GameObject titleObject;
     public GameObject taglineObject;
-    public GameObject startObject;
+    public GameObject startButtonObject;
     public GameObject optionsButtonObject;
 
     [Header("Options Menu")]
     public GameObject optionsPanel;
-    public Slider masterVolumeSlider;
 
     [Header("Sub Menus")]
     public GameObject controlsPanel;
     public GameObject creditsPanel;
 
     private bool starting = false;
-    private bool optionsOpen = false;
-
-    private const string MasterVolumeKey = "MasterVolume";
 
     private void Start()
     {
         titleText.text = "Streetlights";
         taglineText.text = "Get home before dark.";
-        pressAnyKeyText.text = "Press any key to start";
 
         if (optionsPanel != null)
             optionsPanel.SetActive(false);
@@ -42,29 +35,6 @@ public class TitleScreen : MonoBehaviour
 
         if (creditsPanel != null)
             creditsPanel.SetActive(false);
-
-        float savedVolume = PlayerPrefs.GetFloat(MasterVolumeKey, 1f);
-        AudioListener.volume = savedVolume;
-
-        if (masterVolumeSlider != null)
-        {
-            masterVolumeSlider.value = savedVolume;
-            masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
-        }
-    }
-
-    private void Update()
-    {
-        if (starting) return;
-        if (optionsOpen) return;
-
-        // ignore mouse clicks, only keyboard/gamepad.
-        if (Input.anyKeyDown &&
-            !Input.GetMouseButtonDown(0) &&
-            !Input.GetMouseButtonDown(1))
-        {
-            StartGame();
-        }
     }
 
     public void StartGame()
@@ -78,7 +48,6 @@ public class TitleScreen : MonoBehaviour
 
     public void ShowOptions()
     {
-        optionsOpen = true;
 
         if (titleObject != null)
             titleObject.SetActive(false);
@@ -86,8 +55,8 @@ public class TitleScreen : MonoBehaviour
         if (taglineObject != null)
             taglineObject.SetActive(false);
 
-        if (startObject != null)
-            startObject.SetActive(false);
+        if (startButtonObject != null)
+            startButtonObject.SetActive(false);
 
         if (optionsButtonObject != null)
             optionsButtonObject.SetActive(false);
@@ -104,7 +73,6 @@ public class TitleScreen : MonoBehaviour
 
     public void HideOptions()
     {
-        optionsOpen = false;
 
         if (titleObject != null)
             titleObject.SetActive(true);
@@ -112,8 +80,8 @@ public class TitleScreen : MonoBehaviour
         if (taglineObject != null)
             taglineObject.SetActive(true);
 
-        if (startObject != null)
-            startObject.SetActive(true);
+        if (startButtonObject != null)
+            startButtonObject.SetActive(true);
 
         if (optionsButtonObject != null)
             optionsButtonObject.SetActive(true);
@@ -130,7 +98,6 @@ public class TitleScreen : MonoBehaviour
 
     public void ShowControls()
     {
-        optionsOpen = true;
 
         if (optionsPanel != null)
             optionsPanel.SetActive(false);
@@ -144,7 +111,6 @@ public class TitleScreen : MonoBehaviour
 
     public void ShowCredits()
     {
-        optionsOpen = true;
 
         if (optionsPanel != null)
             optionsPanel.SetActive(false);
@@ -158,7 +124,6 @@ public class TitleScreen : MonoBehaviour
 
     public void BackToOptions()
     {
-        optionsOpen = true;
 
         if (optionsPanel != null)
             optionsPanel.SetActive(true);
@@ -168,12 +133,5 @@ public class TitleScreen : MonoBehaviour
 
         if (creditsPanel != null)
             creditsPanel.SetActive(false);
-    }
-
-    public void SetMasterVolume(float volume)
-    {
-        AudioListener.volume = volume;
-        PlayerPrefs.SetFloat(MasterVolumeKey, volume);
-        PlayerPrefs.Save();
     }
 }
